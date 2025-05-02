efe1d1d74a71f06d4cb8f3318807bbfd3bc3aaef
atlasbleed <radiantdhplayer@gmail.com>
2025-05-01 20:26:56 -0500

    Update daddy.lua

--- daddy.lua
+++ daddy.lua
@@ -55,9 +55,9 @@ shared.paradox = {
     ['Hitbox'] = {
         ['Enabled'] = true,
         ['Guns'] = {
-            ['[Double-Barrel SG]'] = { H = 1, W = 1 },
-            ['[Revolver]'] = { H = 0.8 W = 0.7 },
-            ['[Tactical SG]'] = { H = 0.8, W = 0.8 },
+            ['[Double-Barrel SG]'] = { H = 1.12, W = 1.12 },
+            ['[Revolver]'] = { H = 0.93, W = 0.97 },
+            ['[Tactical SG]'] = { H = 1, W = 1 },
             ['[Rifle]'] = { H = 1.2, W = 1.2 }
         }
     },
